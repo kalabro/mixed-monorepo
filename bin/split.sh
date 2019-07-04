b08734b3fa01c251e804a93e5f6f62989c8f83ab
Kate <kalabro@ya.ru>
2019-07-04 15:24:51 +0300

    Added Circle CI config.

---
+++ bin/split.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+function split()
+{
+    SHA1=`./bin/splitsh-lite --prefix=$1`
+    git push $2 "$SHA1:refs/heads/master" -f
+}
+
+function remote()
+{
+    git remote add $1 $2 || true
+}
+
+git pull origin master
+
+remote php git@github.com:kalabro/mixed-monorepo-php.git
+remote js git@github.com:kalabro/mixed-monorepo-js.git
+
+split 'php' php
+split 'js' js
